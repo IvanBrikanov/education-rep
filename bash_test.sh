006d358dbed699c7c03b1750a33fd24bb864894b
ivanbrikanov <shak97@mail.ru>
2024-02-24 22:38:18 +1000

    bash_sq

---
+++ bash_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#тестовый скрипт автоиндек сированиие и комит на уд реп 
+reason="bash_sq"
+git add -A
+git commit -a -m "$reason"
+
+
+
---
+++ sql_tst.sql
@@ -0,0 +1,4 @@
+SELECT *
+  FROM table_1
+ WHERE
+	column_1 = 'test_str';
